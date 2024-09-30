9e0e563716d3fcba6f0ac7147196c6752e85e7b1
Juan Daniel Cardenas Davila <41236643+JuanGit96@users.noreply.github.com>
2024-09-30 00:37:06 -0500

    Delete Vendor directory

--- Vendor/FAQ/view/frontend/web/js/faq.js
+++
@@ -1,9 +0,0 @@
-require([
-    'jquery'
-], function ($) {
-    $(document).ready(function () {
-        $('.contenedor').on('click', function () {
-            $(this).toggleClass('activa');
-        });
-    });
-});
